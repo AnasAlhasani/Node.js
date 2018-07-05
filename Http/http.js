6043e88d58d69ef8f15b55c519a219b07bd4a6cc
Anas Alhasani <anasalhassni@gmail.com>
2018-07-05 16:43:48 +0300

    Added http demo

---
+++ Http/http.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    if (request.url === '/') { 
+        response.write('Hello World!')
+        response.end()
+    }
+})
+
+
+
+server.listen(3000)
+
+console.log('Listening on port 3000 ...')
